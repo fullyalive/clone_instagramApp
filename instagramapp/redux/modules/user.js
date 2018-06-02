22ed892b3bca916df0b2b46d6bd69c763cffd31c
fullyalive <wogud1202@gmail.com>
2018-06-02 16:25:32 +0900

    Persisting the Redux Store

---
+++ instagramapp/redux/modules/user.js
@@ -0,0 +1,29 @@
+// Imports
+
+// Actions
+
+// Action Creators
+
+// API Action
+
+// Initial State
+
+const initialState = {
+  isLoggedIn: false
+};
+
+// Reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+// Reducer Functions
+
+// Exports
+
+// Default Reducer Export
+
+export default reducer;
