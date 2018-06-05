b5dc6a1ed68b8a785d50f29adfc11fe3af900029
fullyalive <wogud1202@gmail.com>
2018-06-05 17:08:53 +0900

    LogInScreen

--- instagramapp/screens/LogInScreen/container.js
+++ instagramapp/screens/LogInScreen/container.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import LogInScreen from "./presenter";
 
 class Container extends Component {
-  static navigations = ({ navigation }) => ({
-    title: "Log In"
-  });
   render() {
     return <LogInScreen />;
   }
