3752e8061b29d3eb59de9145aae74274304ad43f
fullyalive <wogud1202@gmail.com>
2018-06-15 20:19:37 +0900

    Building the Navigation

--- instagramapp/navigation/TabsNavigation.js
+++ instagramapp/navigation/TabsNavigation.js
@@ -2,6 +2,11 @@ import React from "react";
 import { TabNavigator } from "react-navigation";
 import { Ionicons } from "@expo/vector-icons";
 
-const TabsNavigation = TabNavigator() {
-    
-}
+const TabsNavigation = TabNavigator(
+  {},
+  {
+    tabBarPosition: "bottom"
+  }
+);
+
+export default TabsNavigation;
